febb87cbbe966b0bf5bd2fc74983fa8223f161f8
PaulaSalazar08 <paulasm1710@gmail.com>
2025-09-19 10:35:29 +0200

    Par impar

---
+++ Par_impar.py
@@ -0,0 +1,11 @@
+def par_impar():
+    numero=input("Dime un número: ")
+    for cont in range(1,numero+1):
+        if(cont%2==0):
+            print(str(cont)+" PAR")
+        else:
+            print(str(cont)+" IMPAR")
+
+
+
+par_impar()
